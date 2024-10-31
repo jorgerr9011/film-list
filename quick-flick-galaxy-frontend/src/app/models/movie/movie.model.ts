34efa2a307d2dabac5ee26a83ab6469d810c858c
jorgerr9011 <jorge.rodriguez.rivera@udc.es>
2024-10-31 13:04:31 +0100

    Añadidas interfaces (modelos) de películas y series

---
+++ quick-flick-galaxy-frontend/src/app/models/movie/movie.model.ts
@@ -0,0 +1,8 @@
+export interface Movie {
+    id: number,
+    title: string,
+    overview: string,
+    popularity: number,
+    releasedate: Date,
+    poster: string
+}
---
+++ quick-flick-galaxy-frontend/src/app/models/tvshow/tvshow.model.ts
@@ -0,0 +1,8 @@
+export interface TvShow {
+    id: number,
+    title: string,
+    overview: string,
+    popularity: number,
+    releasedate: Date,
+    poster: string
+}
